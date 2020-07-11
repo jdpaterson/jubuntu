9d7bd502d511d908e2b9fcd8d6287546775e4349
jdpaterson <contact@jpatersondev.com>
2020-07-10 21:16:19 -0700

    add strapi

--- setup/n.sh
+++ setup/n.sh
@@ -1,7 +1,7 @@
 curl -L https://raw.githubusercontent.com/tj/n/master/bin/n -o n
 bash n lts
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - 
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 apt update && apt install -y yarn
 yarn global add @rails/actioncable @rails/activestorage @rails/ujs @rails/webpacker turbolinks webpack-dev-server
-npm install -g n gatsby-cli
+npm install -g n gatsby-cli strapi@alpha
